81f63ac216e1e8b8d427b6099b5d9f884e7cd1d0
Gaurav <adarkar.gaurav3317@gmail>
2020-01-08 11:04:27 +0530

    Updated upto 08-01-2020 at 11AM IST

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from flask import Flask, escape, request
+version: '3.1'
 
-app = Flask(__name__)
+services:
 
-@app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+  wordpress:
+    image: wordpress
+    restart: always
+    ports:
+      - 8080:80
+    environment:
+      WORDPRESS_DB_HOST: db
+      WORDPRESS_DB_USER: user
+      WORDPRESS_DB_PASSWORD: password
+      WORDPRESS_DB_NAME: database
+    volumes:
+      - wordpress:/var/www/html
+
+  db:
+    image: mysql:5.7
+    restart: always
+    environment:
+      MYSQL_DATABASE: database
+      MYSQL_USER: user
+      MYSQL_PASSWORD: password
+      MYSQL_RANDOM_ROOT_PASSWORD: '1'
+    volumes:
+      - db:/var/lib/mysql
+
+volumes:
+  wordpress:
+  db:
